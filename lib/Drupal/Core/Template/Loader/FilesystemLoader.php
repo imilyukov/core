ffc2fbff5f031db536c73575b3a9b6aa56808d18
Ivan Milyukov <ivanmilyukov@gmail.com>
2017-10-04 15:36:00 +0300

    Fixed absolute path

--- lib/Drupal/Core/Template/Loader/FilesystemLoader.php
+++ lib/Drupal/Core/Template/Loader/FilesystemLoader.php
@@ -37,7 +37,7 @@ class FilesystemLoader extends \Twig_Loader_Filesystem {
     }
 
     foreach ($namespaces as $name => $path) {
-      $this->addPath($path . '/templates', $name);
+      $this->addPath(DRUPAL_ROOT . '/' . $path . '/templates', $name);
     }
   }
 
